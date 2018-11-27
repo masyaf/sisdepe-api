c06964b9eed8b445d7e463f491add0cebba29450
masya <masya@MASYAF-SAIHTTAM.local>
2018-11-26 22:18:28 -0200

    Creation of Api

--- src/main/java/br/com/sisdepe/api/SisdepeApiApplication.java
+++ src/main/java/br/com/sisdepe/api/SisdepeApiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SisdepeApiApplication {
 
+	//
 	public static void main(String[] args) {
 		SpringApplication.run(SisdepeApiApplication.class, args);
 	}
